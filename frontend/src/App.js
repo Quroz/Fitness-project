1249c62c90852d33d290dac986f37eeeff59f03d
Rakin <54189419+RakinAli@users.noreply.github.com>
2023-09-14 16:04:49 +0200

    Lägg in SVG bilder

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,7 +11,7 @@ function App() {
   return (
 		<div>
 			<NavbarView />
-
+      
 			<BrowserRouter>
 				<Routes>
 					<Route path="/login" element={<Login />} />
--- frontend/src/pages/Navbar.tsx
+++ frontend/src/pages/Navbar.tsx
@@ -7,25 +7,21 @@ You should be able to navigate to the other pages from the navbar.
 The navbar will be a vertical navbar on the left side of the screen.
 */
 
+// Import icons and images
+import dashboard from "../Assets/Navbar/dboard.svg";
+import workout from "../Assets/Navbar/workout.svg";
+import progression from "../Assets/Navbar/progression.svg";
+import explore from "../Assets/Navbar/explore.svg";
+
 function NavbarView() {
 	return (
 		<div className="flex-col flex-auto w-min">
-			<div className="flex-col flex-auto bg-slate-500">       
-				<div>
-					Dashboard
-				</div>
-        <div>
-          Workout Plan
-        </div>
-        <div>
-          Progression 
-        </div>
-        <div>
-          Explore page
-        </div>
-        <div>
-          Settings
-        </div>
+			<div className="flex-col flex-auto bg-slate-500">
+				<div>Dashboard</div>
+				<div>Workout Plan</div>
+				<div>Progression</div>
+				<div>Explore page</div>
+				<div>Settings</div>
 			</div>
 		</div>
 	);
