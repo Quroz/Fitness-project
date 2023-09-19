ac90048329cfd5b4b8653896849e3a5924b0b202
Rakin <54189419+RakinAli@users.noreply.github.com>
2023-09-19 14:20:41 +0200

    More styling
    Better user-experience when clicking and coovering
    
    Changing colors of the buttons

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -6,7 +6,7 @@ You should be able to navigate to the other pages from the navbar.
 /*
 The navbar will be a vertical navbar on the left side of the screen.
 */
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 // Import icons and images
@@ -25,26 +25,66 @@ The navbar will have a width of constant 15% of the screen.
 The navbar will have a height of 100% of the screen.
 
 First div --> flexbox with column direction
-
-
 */
 
-function NavbarView() {
+type Props = {};
+
+function NavbarView({}: Props) {
+	const [thisPage, setThisPage] = useState("dashboard"); // ['dashboard', 'workoutplan', 'progress', 'explore'
+	/*If link is pressed, the icon is to have a darker background */
 	return (
-		<div className="flex flex-col w-20 fixed h-screen bg-lime-300 ">
-			<nav className="flex-col">
-				<Link to="/dashboard">
-					<img src={DashboardIcon} alt="Dashboard Icon" />
-				</Link>
-				<Link to="/workoutplan">
-					<img src={WorkoutPlanIcon} alt="Workout Plan Icon" className="my-2" />
-				</Link>
-				<Link to="/progress">
-					<img src={ProgressionIcon} alt="Progression Icon" className="my-2" />
-				</Link>
-				<Link to="/explore">
-					<img src={ExploreIcon} alt="Explore Icon" className="my-2" />
-				</Link>
+		<div className="flex flex-col h-screen bg-gray-200 w-11 ">
+			<nav className="flex-col object-center mt-20 mr-2">
+				<div className="space-y-4">
+					<div
+						className={`${
+							thisPage === "dashboard" ? "bg-green-100	" : "bg-transparent"
+						} transform transition-transform hover:scale-105`}
+						onClick={() => setThisPage("dashboard")}
+					>
+						<Link to="/dashboard">
+							<img src={DashboardIcon} alt="Dashboard Icon" />
+						</Link>
+					</div>
+					<div
+						className={`${
+							thisPage === "workoutplan" ? "bg-green-100	" : "bg-transparent"
+						} transform transition-transform hover:scale-105`}
+						onClick={() => setThisPage("workoutplan")}
+					>
+						<Link to="/workoutplan">
+							<img
+								src={WorkoutPlanIcon}
+								alt="Workout Plan Icon"
+								className="my-2"
+							/>
+						</Link>
+					</div>
+					<div
+						className={`${
+							thisPage === "progress" ? "bg-green-100" : "bg-transparent"
+						} transform transition-transform hover:scale-105`}
+						onClick={() => setThisPage("progress")}
+					>
+						<Link to="/progress">
+							<img
+								src={ProgressionIcon}
+								alt="Progression Icon"
+								className="my-2"
+							/>
+						</Link>
+					</div>
+					<div
+						className={`${
+							thisPage === "explore" ? "bg-green-100" : "bg-transparent"
+						} transform transition-transform hover:scale-105`}
+						onClick={() => setThisPage("explore")}
+					>
+						<Link to="/explore">
+							<img src={ExploreIcon} alt="Explore Icon" className="my-2" />
+						</Link>
+					</div>
+				</div>
 			</nav>
 		</div>
 	);
