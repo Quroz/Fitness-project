a16cf2b88be6fc7e0a277266b152e5b4ec619fb9
Rakin <54189419+RakinAli@users.noreply.github.com>
2023-09-24 11:41:15 +0200

    Update SearchbarView.tsx

--- frontend/src/pages/Explore/SearchbarView.tsx
+++ frontend/src/pages/Explore/SearchbarView.tsx
@@ -29,29 +29,28 @@ export default function SearchbarView({
 }: SearchbarViewProps) {
 	return (
 		<div>
-			<div className="border border-solid">
-				<Menu>
-					<div className="flex w-30 bg-slate-700">
-						<Menu.Button className="w-fit ">{selectedPart}</Menu.Button>
-					</div>
-					<Menu.Items>
-						{bodyPart.map((bodyArea) => (
-							<Menu.Item key={bodyArea.part}>
-								<button
-									className="flex flex-1"
-									onClick={() => {
-										setSelectedPart(bodyArea.apiCall);
-										Exercise_api.exercise_part(bodyArea.apiCall, 10).then(
-											(data) => console.log(data)
-										);
-									}}
-								>
-									{bodyArea.part}
-								</button>
-							</Menu.Item>
-						))}
-					</Menu.Items>
-				</Menu>
+			<div className="flex flex-1">
+				<div className="w-28 bg-slate-700">
+					<Menu>
+						<div>
+							<Menu.Button className="w-20 ">{selectedPart}</Menu.Button>
+						</div>
+						<Menu.Items>
+							{bodyPart.map((bodyArea) => (
+								<Menu.Item key={bodyArea.part}>
+									<button
+										className="flex flex-1"
+										onClick={() => {
+											setSelectedPart(bodyArea.apiCall);
+										}}
+									>
+										{bodyArea.part}
+									</button>
+								</Menu.Item>
+							))}
+						</Menu.Items>
+					</Menu>
+				</div>
 			</div>
 			<div>
 				<h1>Exercises</h1>
