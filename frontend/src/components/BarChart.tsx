3acc09d40d46dfd824ca41864eefc60c1522524c
Nicolas Salvatore Rollino Subiabre <nsrs@kth.se>
2023-09-26 18:26:23 +0200

    final Dash

--- frontend/src/components/BarChart.tsx
+++ frontend/src/components/BarChart.tsx
@@ -1,12 +1,13 @@
 import React, { useEffect, useRef, useState } from "react";
 import * as d3 from "d3";
 import { select, Selection } from "d3-selection";
+import { text } from "d3";
 interface IProps {
   heightSVG: number;
   widthSVG: number;
 }
 function BarChart({ heightSVG, widthSVG }: IProps) {
-  let margin = {top: 10, right: 30, bottom: 90, left: 40},
+  let margin = { top: 10, right: 30, bottom: 90, left: 40 },
     width = widthSVG - margin.left - margin.right,
     height = heightSVG - margin.top - margin.bottom;
   const data = [
@@ -49,9 +50,9 @@ function BarChart({ heightSVG, widthSVG }: IProps) {
     if (!svg) {
       setSelection(select(svgRef.current));
     } else {
-      const selection = svg.append("g")
-    .attr("transform",
-          "translate(" + margin.left + "," + margin.top + ")");
+      const selection = svg
+        .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
       selection
         .selectAll("rect")
@@ -63,9 +64,9 @@ function BarChart({ heightSVG, widthSVG }: IProps) {
         .attr("y", (d) => yAxis(d.workouts))
         .attr("fill", "#D8FFF8")
         .attr("height", (d) => height - yAxis(d.workouts))
-        .attr("rx", "10")
-      
-        selection
+        .attr("rx", "10");
+
+      selection
         .selectAll("text")
         .data(data)
         .enter()
@@ -73,28 +74,40 @@ function BarChart({ heightSVG, widthSVG }: IProps) {
         .text((d) => d.workouts) // Assuming you want to display the workouts data as labels
         .attr("x", (d) => xAxis(d.week)! + xAxis.bandwidth() / 2) // Position text in the middle of the rectangle
         .attr("y", (d) => yAxis(d.workouts) - 5) // Adjust the y position for better placement
-        .attr("text-anchor", "middle") // Center the text horizontally
-        .attr("fill", "black"); // Set the text color to black (you can change this as needed)
-
-      selection.append("text")
-        .attr("x", width/2)
-        .attr("y", margin.top)
-        .attr("text-anchor", "middle")  
-        .style("font-size", "16px") 
-        .style("text-decoration", "underline")  
-        .text("Workouts in the last 4 weeks");
-
+        .attr("text-anchor", "middle"); // Center the text horizontally
 
       selection
+        .append("text")
+        .attr("x", width / 2)
+        .attr("y", margin.top)
+        .attr("text-anchor", "middle")
+        .style("font-size", "16px")
+        .style("text-decoration", "underline")
+        .text("Workouts in the last 4 weeks");
+
+      const xLine = selection
         .append("g")
-        .attr("fill", "#fafafa")
         .attr("transform", "translate(0," + height + ")")
-        .call(d3.axisBottom(xAxis))
-   
+        .call(d3.axisBottom(xAxis));
+
+      xLine
+        .selectAll("path") // Select the line element within the axis
+        .style("stroke", "#fafafa"); // Set the stroke color to white
+
+      xLine.selectAll("line").style("stroke", "#fafafa"); // Set the stroke color to white
+
+      xLine.selectAll("text").style("fill", "#fafafa") // Set text to white
     }
   }, [svg]);
 
-  return <svg className="fill-[#FAFAFA] font-bold" ref={svgRef} height={heightSVG} width={widthSVG} />;
+  return (
+    <svg
+      className="fill-[#FAFAFA] font-bold"
+      ref={svgRef}
+      height={heightSVG}
+      width={widthSVG}
+    />
+  );
 }
 
 export default BarChart;
--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -1,17 +1,15 @@
 import React from "react";
 import CircleBar from "../components/CircleBar";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faDumbbell } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faDumbbell } from "@fortawesome/free-solid-svg-icons";
 import BarChart from "../components/BarChart";
-
-
 
 function Dashboard() {
   const innerWidth = window.innerWidth;
   return (
     <div className="flex h-full w-full justify-center flex-col">
       <div className="flex justify-center mt-6 h-2/5 items-start">
-        <div className="justify-around items-center flex h-4/5 w-4/5 rounded-3xl bg-[#716FFF]">
+        <div className="justify-around items-center flex h-4/5 w-4/5 rounded-3xl bg-[#716FFF] shadow-[20px_20px_25px_-20px]">
           <CircleBar widthSVG={200} heightSVG={200} workouts={3} target={4} />
           <div className="w-2/6  h-full flex flex-col justify-center">
             <text className="font-bold text-[#FAFAFA] text-3xl">
@@ -23,14 +21,23 @@ function Dashboard() {
           </div>
         </div>
       </div>
-      <div className="flex justify-center items-center h-3/5">
-        <div className="bg-[#716FFF] h-4/5 w-1/5 rounded-3xl flex  flex-col">
-        <text className="font-bold text-[#FAFAFA] text-3xl mx-14 mt-14">Your favourite workout is Back Day 2</text>
-        
-          <FontAwesomeIcon color="#fafafa" icon={faDumbbell} />
+      <div className="flex justify-center items-center h-3/5 ">
+        <div className="bg-[#716FFF] h-4/5 w-1/5 rounded-3xl flex  flex-col shadow-[20px_20px_25px_-20px]">
+          <text className="h-3/5 font-bold text-[#FAFAFA] text-3xl mx-14 mt-14">
+            Your favourite workout is Back Day 2
+          </text>
+
+          <div className="h-2/5 flex justify-end items-center me-4">
+            <FontAwesomeIcon  style={{
+              width: "80.378px",  
+              height: "43.649px",
+              transform: "rotate(-41.5deg)",
+              
+            }} color="#fafafa" icon={faDumbbell} />
+          </div>
         </div>
-        <div className="bg-[#716FFF] ml-10 rounded-3xl w-3/5 h-4/5 flex justify-center items-center">
-          <BarChart widthSVG={innerWidth * 3/5-150} heightSVG={350}/>
+        <div className="shadow-[20px_20px_25px_-20px] bg-[#716FFF] ml-10 rounded-3xl w-3/5 h-4/5 flex justify-center items-center">
+          <BarChart widthSVG={(innerWidth * 3) / 5 - 150} heightSVG={350} />
         </div>
       </div>
     </div>
