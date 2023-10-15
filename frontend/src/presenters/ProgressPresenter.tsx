f058b9ddd51f77e637af693a74b928338fa704b9
Rakin <54189419+RakinAli@users.noreply.github.com>
2023-10-15 23:35:47 +0200

    Lazy rendering explorePage

--- frontend/src/presenters/ProgressPresenter.tsx
+++ frontend/src/presenters/ProgressPresenter.tsx
@@ -42,46 +42,48 @@ export const ProgressPresenter = () => {
 	console.log("workouts test", workouts);
 
 	async function finishWorkout() {
-		const response = await fetch("http://localhost:4000/api/workout/addCompletedWorkout", {
-			method: "POST",
-			headers: {
-				"Content-Type": "application/json",
-				Authorization: `Bearer ${user}`,
-			},
-			body: JSON.stringify({
-				plan_id: dataJSON.id,
-				workout: currentWorkout,
-				date: new Date(),
-			}),
-		});
+		const response = await fetch(
+			"http://localhost:4000/api/workout/addCompletedWorkout",
+			{
+				method: "POST",
+				headers: {
+					"Content-Type": "application/json",
+					Authorization: `Bearer ${user}`,
+				},
+				body: JSON.stringify({
+					plan_id: dataJSON.id,
+					workout: currentWorkout,
+					date: new Date(),
+				}),
+			}
+		);
 		const data = await response.json();
-		
-		if(response.status !== 200){
-			alert("Something went wrong, please try again")
-		}
-		else{
-			alert("Workout completed")
-			window.location.reload()
+
+		if (response.status !== 200) {
+			alert("Something went wrong, please try again");
+		} else {
+			alert("Workout completed");
+			window.location.reload();
 		}
 	}
 
-	
 	useEffect(() => {
 		let copy: Workout[] = new Array(workouts.length);
 
 		for (let index = 0; index < workouts.length; index++) {
-
-	
 			copy[index] = {
 				name: workouts[index].name,
-                equipment: workouts[index].equipment,
-                bodyPart: workouts[index].bodyPart,
-                sets: workouts[index].sets,
-                reps: workouts[index].reps,
-                completedSets: []
+				equipment: workouts[index].equipment,
+				bodyPart: workouts[index].bodyPart,
+				sets: workouts[index].sets,
+				reps: workouts[index].reps,
+				completedSets: [],
 			};
-			for(let i = 0; i< workouts[index].sets; i++){
-				copy[index].completedSets.push({reps: workouts[index].reps, weight: 0});
+			for (let i = 0; i < workouts[index].sets; i++) {
+				copy[index].completedSets.push({
+					reps: workouts[index].reps,
+					weight: 0,
+				});
 			}
 		}
 
@@ -134,15 +136,14 @@ export const ProgressPresenter = () => {
 			);
 		});
 	}
-	function handleExcerciseChange(id:number){
-		if(id < 0) setCurrent(0);
-		else if( id > currentWorkout.length-1) setCurrent(currentWorkout.length-1);
-		else{
-		  setCurrent(id);
+	function handleExcerciseChange(id: number) {
+		if (id < 0) setCurrent(0);
+		else if (id > currentWorkout.length - 1)
+			setCurrent(currentWorkout.length - 1);
+		else {
+			setCurrent(id);
 		}
-		
-		
-	  }
+	}
 
 	return (
 		<Progress
