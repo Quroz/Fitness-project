b912207dd1bb5372d2c582e82e5ff06d04107fb9
Rakin <54189419+RakinAli@users.noreply.github.com>
2023-10-15 16:38:07 +0200

    Code Cleaning
    Co-Authored-By: Filip Wetterdal Todorovic <81308705+Quroz@users.noreply.github.com>

--- frontend/src/presenters/Explore.tsx
+++ frontend/src/presenters/Explore.tsx
@@ -9,87 +9,85 @@ import EquipmentList from "../interfaces/Equipment";
 import { useNavigate } from "react-router-dom";
 
 function ExplorePresenter() {
-  // Related to body part filter or search
-  const [selectedPart, setSelectedPart] = useState("Select a body part");
-  const [exerciseData, setExerciseData] = useState<Exercise[]>([]);
+	// Related to body part filter or search
+	const [selectedPart, setSelectedPart] = useState("Select a body part");
+	const [exerciseData, setExerciseData] = useState<Exercise[]>([]);
 
-  // Related to how many exercises are shown
-  const [exercisesShown, setExercisesShown] = useState(0);
+	// Related to how many exercises are shown
+	const [exercisesShown, setExercisesShown] = useState(0);
 
-  // Related to equipment filter
-  const [equipments, setEquipments] = useState("Select an equipment");
-  const [filterbyEquipment, setFilterbyEquipment] = useState(false);
+	// Related to equipment filter
+	const [equipments, setEquipments] = useState("Select an equipment");
+	const [filterbyEquipment, setFilterbyEquipment] = useState(false);
 
-  // Related to searching exercise by name
-  const [searchExercise, setSearchExercise] = useState("");
-  const [searchByName, setSearchByName] = useState(1);
+	// Related to searching exercise by name
+	const [searchExercise, setSearchExercise] = useState("");
+	const [searchByName, setSearchByName] = useState(1);
 
-  const [showLoading, setShowLoading] = useState(false);
+	const [showLoading, setShowLoading] = useState(false);
 
-  // Related to navigating to instructions page
-  const navigate = useNavigate();
+	// Related to navigating to instructions page
+	const navigate = useNavigate();
 
-  useEffect(() => {
-    if (searchExercise !== "") {
-      setShowLoading(true);
-      Exercise_api.exercise_name(searchExercise, exercisesShown)
-        .then((data) => setExerciseData(data))
-        .finally(() => {
-          setShowLoading(false);
-        });
-    }
+	useEffect(() => {
+		if (searchExercise !== "") {
+			setShowLoading(true);
+			Exercise_api.exercise_name(searchExercise, exercisesShown)
+				.then((data) => setExerciseData(data))
+				.finally(() => {
+					setShowLoading(false);
+				});
+		}
 
-    if (selectedPart === "Select a body part") return;
-    Exercise_api.exercise_part(selectedPart, exercisesShown)
-      .then((data) => {
-        setShowLoading(true);
-        setExerciseData(data);
-      })
-      .finally(() => {
-        setShowLoading(false);
-      });
-  }, [exercisesShown, selectedPart, searchByName]);
+		if (selectedPart === "Select a body part") return;
+		Exercise_api.exercise_part(selectedPart, exercisesShown)
+			.then((data) => {
+				setShowLoading(true);
+				setExerciseData(data);
+			})
+			.finally(() => {
+				setShowLoading(false);
+			});
+	}, [exercisesShown, selectedPart, searchByName]);
 
-  function gotoInstructionsPage(exercise: Exercise) {
-    const data = { exercise: exercise };
-    const queryParam = encodeURIComponent(JSON.stringify(data));
-    navigate(`/instructions?data=${queryParam}`);
-  }
+	function gotoInstructionsPage(exercise: Exercise) {
+		const data = { exercise: exercise };
+		const queryParam = encodeURIComponent(JSON.stringify(data));
+		navigate(`/instructions?data=${queryParam}`);
+	}
 
-  
-
-  return (
-    <div className="flex flex-col w-full h-screen bg-gray-100 first-letter">
-      <div className="w-full py-1 mt-8">
-        <h1 className="text-5xl text-center">Explore Page</h1>
-      </div>
-      <SearchbarView
-        // Related to filtering and searching by body part
-        bodyPart={bodyPart}
-        selectedPart={selectedPart}
-        setSelectedPart={setSelectedPart}
-        exercise_results={exerciseData}
-        // Related to how many exercises are shown
-        setExercisesShown={setExercisesShown}
-        exercisesShown={exercisesShown}
-        // Related to filtering by equipment
-        equipments={equipments}
-        equipmentList={EquipmentList}
-        setEquipments={setEquipments}
-        filterbyEquipment={filterbyEquipment}
-        setFilterbyEquipment={setFilterbyEquipment}
-        // Related to searching exercise by name
-        searchExercise={searchExercise}
-        setSearchExercise={setSearchExercise}
-        searchByName={searchByName}
-        setSearchByName={setSearchByName}
-        // Related to navigating to instructions page
-        goToInstructionsPage={gotoInstructionsPage}
-        showLoading={showLoading}
-        setShowLoading={setShowLoading}
-      />
-    </div>
-  );
+	return (
+		<div className="flex flex-col w-full h-screen bg-gray-100 first-letter">
+			<div className="w-full py-1 mt-8">
+				<h1 className="text-5xl text-center">Explore Page</h1>
+			</div>
+			<SearchbarView
+				// Related to filtering and searching by body part
+				bodyPart={bodyPart}
+				selectedPart={selectedPart}
+				setSelectedPart={setSelectedPart}
+				exercise_results={exerciseData}
+				// Related to how many exercises are shown
+				setExercisesShown={setExercisesShown}
+				exercisesShown={exercisesShown}
+				// Related to filtering by equipment
+				equipments={equipments}
+				equipmentList={EquipmentList}
+				setEquipments={setEquipments}
+				filterbyEquipment={filterbyEquipment}
+				setFilterbyEquipment={setFilterbyEquipment}
+				// Related to searching exercise by name
+				searchExercise={searchExercise}
+				setSearchExercise={setSearchExercise}
+				searchByName={searchByName}
+				setSearchByName={setSearchByName}
+				// Related to navigating to instructions page
+				goToInstructionsPage={gotoInstructionsPage}
+				showLoading={showLoading}
+				setShowLoading={setShowLoading}
+			/>
+		</div>
+	);
 }
 
 export default ExplorePresenter;
